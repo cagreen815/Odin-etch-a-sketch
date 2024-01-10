abf97e064fe832bb913354c4a356aa3dca015d2d
cagreen815 <62021837+cagreen815@users.noreply.github.com>
2024-01-10 14:29:00 -0500

    added additional lines to main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    createBoard(16);
+    console.log('HI')
+} )
+
+function createBoard(size){
+    let board = document.querySelector('.container');
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size; 
+
+    for(let i = 0; i < numDivs; i++) {
+        let div = document.createElement('div');
+        div.style.backgroundColor = 'yellow';
+        board.insertAdjacentElement('beforeend', div)
+    }
+}
